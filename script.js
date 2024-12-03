6659d44c488e37354eafa61c5d074de063c266f9
joelepore <giuseppejoelepore@gmail.com>
2024-12-03 13:26:44 +0100

    feat: complete base exercise

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
+// Soluzione 1
+const emails = [];
+
+const checkAndAddEmailsToDOM = () => {
+  if (emails.length === 10) {
+    emails.forEach(email => {
+      const li = document.createElement('li');
+      li.innerText = email;
+      document.querySelector('ul').append(li);
+    });
+
+  }
+}
+
+const fetchEmails = (numEmails) => {
+  for (let i = 0; i < numEmails; i++) {
+    axios.get(endpoint)
+      .then(({ data }) => {
+        emails.push(data.response);
+        checkAndAddEmailsToDOM();
+      })
+  }
+}
+
+fetchEmails(10);
